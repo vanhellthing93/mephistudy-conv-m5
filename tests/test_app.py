38a281fe124dac394af48aa9effd62f761731ccc
vanhellthing <vanhellthing@mail.ru>
2024-11-25 17:11:31 +0300

    app+tests

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+import pytest
+from inspect import getsourcefile
+import os.path
+import sys
+
+current_path = os.path.abspath(getsourcefile(lambda:0))
+current_dir = os.path.dirname(current_path)
+parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
+
+sys.path.insert(0, parent_dir)
+
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello_world(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello, World!' in response.data

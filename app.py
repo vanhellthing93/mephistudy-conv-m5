64c0a391e24c537894e23095f2de4d5aa8ea961b
vanhellthing <vanhellthing@mail.ru>
2024-11-26 15:50:54 +0300

    Changed server ip

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!!!!!!!!!!!!!!!!!</p>"
+    return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)

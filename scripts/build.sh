1fde6d6906df3d1fb3f60fd56866e2ac6c35f2f4
vanhellthing <vanhellthing@mail.ru>
2024-11-26 16:32:01 +0300

    added build stage

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone https://github.com/vanhellthing93/mephistudy-conv-m5
+
+cd mephistudy-conv-m5
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install -r requirements.txt

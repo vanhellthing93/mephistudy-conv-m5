02ada7b13d7e25a9318fa94ec5ea90eaa24e5080
vanhellthing <vanhellthing@mail.ru>
2024-11-26 16:39:59 +0300

    testing 2

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#git clone https://github.com/vanhellthing93/mephistudy-conv-m5
+git clone https://github.com/vanhellthing93/mephistudy-conv-m5
 
 cd mephistudy-conv-m5
 
@@ -8,3 +8,5 @@ python3 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
+
+python app.py

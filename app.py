70b5d847d26036c472909404175f6f3c804735e5
vanhellthing <vanhellthing@mail.ru>
2025-04-25 17:58:21 +0300

    tr

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Test CI 2/CD</p>"
+    return "<p>Test CI 2/CD 2 </p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
